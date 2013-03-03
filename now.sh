5e53a5823e267089ca672366c2a9c39bfc0a90e0
Alex Pankratov <ap@swapped.cc>
2013-03-03 16:13:11 +0100

    changes

---
+++ now.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#
+#	see what 'read -t' returns on timeout
+#
+> ~/.now.sh.tmp
+read -t 1 < ~/.now.sh.tmp
+timeout=$?
+rm ~/.now.sh.tmp
+
+#
+#	loop forever
+#
+while true
+do
+	#
+	#	relay stdin
+	#
+        while true
+        do
+                read -t 1 line; rc=$?
+                if [ $rc != 0 ]; then break; fi
+                echo $line
+        done
+        if [ $rc != $timeout ]; then break; fi
+	
+	#
+	#	print timestamp
+	#
+	if [ $# -eq 1 ]; then 
+		now=`date +"$1"`
+	else
+		now=`date`
+	fi
+        echo -ne "$now\r"
+done
