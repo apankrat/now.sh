f629cb645fff8f47379a7bdd01b89ea183ad348e
Alex Pankratov <ap@swapped.cc>
2013-03-13 10:11:51 +0100

    Restore timeout checking logic
    Because of this -
    
    mx:~# bash --version
    GNU bash, version 3.1.17(1)-release (i486-pc-linux-gnu)
    Copyright (C) 2005 Free Software Foundation, Inc.
    
    mx:~# read -r -t 1; echo $?
    1

--- now.sh
+++ now.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 #
+#  See what 'read -t' returns on timeout.
+#
+#  For that - read from stdout. Alternatively, it could read
+#  from /dev/zero, but it's not available under Cygwin and
+#  in other non-*nix environments.
+#
+#  Also note, that while bash manual states that the return
+#  code on a timeout is greater than 128, it doesn't hold
+#  true with a number of bash implementations, most notably
+#  including GNU bash that returns 1 instead.
+#
+read -t 1 <&1
+timeout=$?
+
+#
 #	loop forever
 #
 while true
@@ -17,7 +32,7 @@ do
 	#
 	#	exit status is greater than 128 if the timeout is exceeded
 	#
-	if [ $rc -le 128 ]; then break; fi
+	if [ $rc != $timeout ]; then break; fi
 
 	#
 	#	print the timestamp
